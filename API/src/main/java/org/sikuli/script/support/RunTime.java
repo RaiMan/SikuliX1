0e60601114b55a017156dea01cad4e4410a348a0
Matthias Balmer <mbalmer@smss.ch>
2019-06-24 11:47:20 +0200

    Pass user args to Runner.runScripts when running with -r

--- API/src/main/java/org/sikuli/script/support/RunTime.java
+++ API/src/main/java/org/sikuli/script/support/RunTime.java
@@ -191,7 +191,7 @@ public class RunTime {
     }
 
     if (runningScripts()) {
-      int exitCode = Runner.runScripts(RunTime.getRunScripts(), null, new IScriptRunner.Options());
+      int exitCode = Runner.runScripts(RunTime.getRunScripts(), userArgs, new IScriptRunner.Options());
       if (exitCode > 255) {
         exitCode = -1;
       }
