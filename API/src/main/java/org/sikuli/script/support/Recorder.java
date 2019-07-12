9c05dc8edf4141ea0107b0f86d6181e6d84a1d4a
Matthias Balmer <mbalmer@smss.ch>
2019-07-12 10:14:36 +0200

    Some cleanup

--- API/src/main/java/org/sikuli/script/support/Recorder.java
+++ API/src/main/java/org/sikuli/script/support/Recorder.java
@@ -1,19 +1,10 @@
 package org.sikuli.script.support;
 
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Field;
 import java.nio.file.Files;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
-import java.util.TreeSet;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -22,9 +13,6 @@ import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.imageio.ImageIO;
-
-import org.apache.commons.io.FileUtils;
 import org.jnativehook.GlobalScreen;
 import org.jnativehook.NativeHookException;
 import org.jnativehook.NativeInputEvent;
@@ -33,27 +21,10 @@ import org.jnativehook.keyboard.NativeKeyListener;
 import org.jnativehook.mouse.NativeMouseEvent;
 import org.jnativehook.mouse.NativeMouseListener;
 import org.jnativehook.mouse.NativeMouseMotionListener;
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import org.opencv.core.MatOfPoint;
-import org.opencv.core.MatOfRect;
-import org.opencv.core.Point;
-import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
-import org.opencv.features2d.MSER;
-import org.opencv.imgcodecs.Imgcodecs;
-import org.opencv.imgproc.Imgproc;
-import org.opencv.objdetect.Objdetect;
-import org.sikuli.script.Finder;
 import org.sikuli.script.Screen;
 import org.sikuli.script.ScreenImage;
-import org.sikuli.script.support.generators.ICodeGenerator;
 import org.sikuli.script.support.recorder.RecordedEventsFlow;
 import org.sikuli.script.support.recorder.actions.IRecordedAction;
-import org.sikuli.script.Finder.Finder2;
-import org.sikuli.script.Key;
-import org.sikuli.script.Match;
 
 public class Recorder implements NativeKeyListener, NativeMouseListener, NativeMouseMotionListener {
 
@@ -121,21 +92,19 @@ public class Recorder implements NativeKeyListener, NativeMouseListener, NativeM
   private final ExecutorService executor = Executors.newSingleThreadExecutor();
 
   private void screenshot(boolean debounce) {
-    if (debounce) {
-      debouncer.debounce(new Runnable() {
-        @Override
-        public void run() {
-          ScreenImage img = Screen.getPrimaryScreen().capture();
-          String file = img.save(screenshotDir.getAbsolutePath());
-          eventsFlow.addScreenshot(file);
-        }
-      }, SCREENSHOT_DEBOUNCE_MILLIS, TimeUnit.MILLISECONDS);
-    }else {
-      executor.execute(() -> {
+    Runnable runnable = new Runnable() {
+      @Override
+      public void run() {
         ScreenImage img = Screen.getPrimaryScreen().capture();
         String file = img.save(screenshotDir.getAbsolutePath());
         eventsFlow.addScreenshot(file);
-      });
+      }
+    };    
+    
+    if (debounce) {
+      debouncer.debounce(runnable, SCREENSHOT_DEBOUNCE_MILLIS, TimeUnit.MILLISECONDS);
+    }else {
+      executor.execute(runnable);
     }
   }
 
