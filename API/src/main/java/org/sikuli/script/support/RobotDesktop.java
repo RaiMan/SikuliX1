a4e751898666a10e35706ad41ab4049ad186c5da
Matthias Balmer <mbalmer@smss.ch>
2020-01-30 08:09:56 +0100

    Fix ConcurrentModificationException in RobotDesktop.keyUp()

--- API/src/main/java/org/sikuli/script/support/RobotDesktop.java
+++ API/src/main/java/org/sikuli/script/support/RobotDesktop.java
@@ -364,7 +364,7 @@ public class RobotDesktop extends Robot implements IRobot {
   @Override
   public void keyUp() {
     keyUp(heldKeys);
-    for (int code : heldKeyCodes) {
+    for (int code : new ArrayList<>(heldKeyCodes)) {
       keyUp(code);
     }
   }
