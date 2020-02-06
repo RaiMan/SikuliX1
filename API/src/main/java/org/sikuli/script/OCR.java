c44d5061f8d6fd54e58742c7c10e0c4a14163282
RaiMan <sikulix@outlook.com>
2020-02-06 11:25:43 +0100

    javadocs [ci skip]

--- API/src/main/java/org/sikuli/script/OCR.java
+++ API/src/main/java/org/sikuli/script/OCR.java
@@ -283,8 +283,8 @@ public class OCR {
   /**
    * A container for the options relevant for using {@link OCR} on
    * {@link Region} or {@link Image}.
-   * <p>Use {@link OCR.Options#Options()} to get a new option set</p>
-   * <p>use {@link OCR#globalOptions} to access the global options</p>
+   * <p>Use {@link OCR.Options} to get a new option set</p>
+   * <p>Use {@link OCR#globalOptions} to access the global options</p>
    * <p>
    * In case you have to consult the Tesseract docs
    * @see <a href="https://github.com/tesseract-ocr/tesseract/wiki/Documentation">Tesseract docs</a>
