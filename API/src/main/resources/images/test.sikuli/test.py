b9602ad26b6a33c5eccd6773101f6c6709832b5b
RaiMan <sikulix@outlook.com>
2020-03-03 11:39:24 +0100

    ongoing: auto-resize [ci skip]

--- API/src/main/resources/images/test.sikuli/test.py
+++ API/src/main/resources/images/test.sikuli/test.py
@@ -1,16 +1,5 @@
+"house1_mac.png"
 img = "img.png"
-
-match = find(img)
-match.highlight(2)
-exit()
-
-img = r"C:\Users\rmhde\IdeaProjects\SikuliX1\API\src\main\resources\images\_backUp\test"
-
-simg = SCREEN.cmdCapture(SCREEN)
-print "capture:", simg
-finder = Finder(simg, Region(0,0,1,1))
-finder.find(img)
-if finder.hasNext():
-  finder.next().highlight(2)
-
-pattern = Pattern("pattern.png").exact().targetOffset(49,38)
+print find(img)
+imgSmall = "imgSmall.png" 
+print find(imgSmall)
