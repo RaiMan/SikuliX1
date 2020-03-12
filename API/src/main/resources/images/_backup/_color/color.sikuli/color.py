37eab0d652adaa5610cfad23173aca7db799ddfb
RaiMan <rmhdevelop@me.com>
2020-03-12 15:52:31 +0100

    added: images [ci skip]

--- API/src/main/resources/images/_backup/_color/color.sikuli/color.py
+++ API/src/main/resources/images/_backup/_color/color.sikuli/color.py
@@ -3,22 +3,24 @@ img1 = "source_activate.jpg"
 img2 = "source_activated.jpg"
 button = "buttonactivate.png"
 
+"""
 m = find(button)
 m.highlight(2)
 
 exit()
+"""
 
 ib = Finder(Image.create(button))
 ib.find(button)
-print ib.next().getScore()
+print "button:", ib.next().getScore()
 ib = Finder(Image.create(img1))
 ib.find(button)
-print ib.next().getScore()
+print "img1:", ib.next().getScore()
 ib = Finder(Image.create(img2))
 ib.find(button)
-print ib.next().getScore()
-
+print "img2:", ib.next().getScore()
 """
+
 print "button:", Image(button).find(button)
 print "img1:", Image(img1).find(button)
 print "img2:", Image(img2).find(button)
