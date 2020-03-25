80ba797529defbcae1e9f5a0afcb305123ed8a50
RaiMan <sikulix@outlook.com>
2020-03-25 16:36:46 +0100

    added: tests for RunTime features

---
+++ API/src/test/java/org/sikuli/script/test/RunTimeTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2010-2020, sikuli.org, sikulix.com - MIT license
+ */
+
+package org.sikuli.script.test;
+
+import org.junit.*;
+import org.junit.runners.MethodSorters;
+import org.sikuli.script.support.ExtensionManager;
+import org.sikuli.script.support.RunTime;
+import org.sikuli.script.support.SXTest;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class RunTimeTest extends SXTest {
+
+  @Before
+  public void setUp() {
+    setUpBase();
+  }
+
+  @Test
+  public void test000_StartUp() {
+    startUpBase();
+  }
+
+  @Ignore
+  public void test001_Playground() {
+    testIntro();
+    //Assume.assumeFalse("Running headless - ignoring test", RunTime.isHeadless());
+    RunTime.pause(3);
+    testOutro("");
+  }
+
+  @Test
+  public void test010_ExtensionsFile() {
+    testIntro();
+    ExtensionManager.readExtensions(false);
+    testOutro("");
+    //assertTrue("NotValid: " + image.toString(), image.isValid());
+  }
+}
