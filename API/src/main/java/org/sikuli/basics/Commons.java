257427e07054c8f8506e4b5bfb0679cd2dab55c7
RaiMan <sikulix@outlook.com>
2020-07-31 12:44:46 +0200

    validate/make loadable: opencv/tesseract libs on Mac/Linux

--- API/src/main/java/org/sikuli/basics/Commons.java
+++ API/src/main/java/org/sikuli/basics/Commons.java
@@ -1,6 +1,20 @@
 package org.sikuli.basics;
 
 public class Commons {
+  private static final String osVersionSysProp = System.getProperty("os.version");
+
+  public static boolean runningWindows() {
+    return osVersionSysProp.startsWith("windows");
+  }
+
+  public static boolean runningMac() {
+    return osVersionSysProp.startsWith("mac");
+  }
+
+  public static boolean runningLinux() {
+    return !runningMac() && !runningWindows();
+  }
+
   public static int[] reverseIntArray(int[] anArray) {
     for(int i=0; i<anArray.length/2; i++){
       int temp = anArray[i];
