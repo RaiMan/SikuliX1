b770d2dd761471f80e1957790e563e0a8a256327
RaiMan <sikulix@outlook.com>
2021-04-04 15:36:14 +0200

    cosmetics

--- Support/commands/win/runide16.cmd
+++ Support/commands/win/runide16.cmd
@@ -2,6 +2,5 @@
 SETLOCAL ENABLEEXTENSIONS
 set ide=C:\Users\rmhde\IdeaProjects\SikuliX1\IDE\target\sikulixide-2.0.6-SNAPSHOT-complete-win.jar
 set java="C:\Program Files\AdoptOpenJDK\jdk16\bin\java.exe"
-echo running %ide%
 %java% -jar %ide% %*
 ENDLOCAL
