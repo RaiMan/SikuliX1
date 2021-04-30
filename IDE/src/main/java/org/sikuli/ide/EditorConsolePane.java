33d3833ef4b2040f4b6f907d6fabe861691c97ac
RaiMan <sikulix@outlook.com>
2021-04-30 14:31:54 +0200

    changed: script runner to be moved to IDE

--- IDE/src/main/java/org/sikuli/ide/EditorConsolePane.java
+++ IDE/src/main/java/org/sikuli/ide/EditorConsolePane.java
@@ -36,8 +36,7 @@ import org.sikuli.basics.Debug;
 //
 // RJHM van den Bergh , rvdb@comweb.nl
 import org.sikuli.basics.PreferencesUser;
-import org.sikuli.script.SX;
-import org.sikuli.script.support.IScriptRunner;
+import org.sikuli.script.runnerSupport.IScriptRunner;
 import org.sikuli.script.support.Runner;
 
 public class EditorConsolePane extends JPanel implements Runnable {
