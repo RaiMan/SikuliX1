f0210e754f39b227b317f061db261a4abbfb30d2
RaiMan <sikulix@outlook.com>
2021-04-30 14:17:35 +0200

    changed: Lib folder to be moved to IDE

--- API/src/main/resources/Lib/guide/__init__.py
+++
@@ -1,4 +0,0 @@
-#  Copyright (c) 2010-2020, sikuli.org, sikulix.com - MIT license
-
-from guide import *
-
--- API/src/main/resources/Lib/xlutils/__init__.py
+++
@@ -1,3 +0,0 @@
-# This Software is released under the MIT License:
-# http://www.opensource.org/licenses/mit-license.html
-# See license.txt for more details.
