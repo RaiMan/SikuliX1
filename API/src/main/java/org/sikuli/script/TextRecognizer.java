d7ee5d49df6c302cb0a65399c6cd949a00132c36
Raimund Hocke <sikulix@outlook.com>
2021-11-29 09:08:59 +0100

    upgrade: Tess4J 4.6.0

--- API/src/main/java/org/sikuli/script/TextRecognizer.java
+++ API/src/main/java/org/sikuli/script/TextRecognizer.java
@@ -40,8 +40,8 @@ public class TextRecognizer {
 
   private static int lvl = 3;
 
-  private static final String versionTess4J = "4.5.4";
-  private static final String versionTesseract = "4.1.x";
+  private static final String versionTess4J = "4.6.0";
+  private static final String versionTesseract = "4.1.3";
 
   private OCR.Options options;
 
