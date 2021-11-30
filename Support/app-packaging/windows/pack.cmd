f848c1fb4f9c2ff98bf7e3a67e5b5e0ccca2a41d
rmhde <rmhshopping@mac.com>
2021-11-30 17:45:45 +0100

    implementing: app packaging using jpackage

--- Support/app-packaging/windows/pack.cmd
+++ Support/app-packaging/windows/pack.cmd
@@ -1,10 +1,17 @@
 del *.exe
-java -version
-jpackage --input src ^
---app-version 2.0.6 ^
+del *.msi
+set version=2.0.6-SNAPSHOT
+jpackage --type msi ^
+--input app-package\jar ^
+--dest app-package\app ^
+--app-version 206.000 ^
 --name SikulixIDE ^
 --icon sikulix.ico ^
---main-jar sikulixide-2.0.6.jar ^
+--description "Visual automation and testing - SikuliX IDE - edit and run scripts" ^
+--vendor "SikuliX - RaiMan" ^
+--copyright "Copyright 2021, MIT License" ^
+--license-file app-package\license.txt ^
+--main-jar sikulixide-%version%.jar ^
 --main-class org.sikuli.ide.Sikulix ^
 --win-shortcut ^
 --win-dir-chooser ^
---
+++ Support/app-packaging/windows/prepare.cmd
@@ -0,0 +1,5 @@
+set version=2.0.6-SNAPSHOT
+set licensedir=..\..\..\IDE\src\main\resources\Settings\LicenseIDE
+copy %licensedir%\LICENSE + %licensedir%\THIRD-PARTY.txt app-package\license.txt
+set jardir=..\..\..\IDE\target
+copy %jardir%\*-win.jar /B app-package\jar\sikulixide-%version%.jar /B
