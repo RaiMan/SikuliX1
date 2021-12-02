cde45bc3fcffcf6d50be8ae24e412700c0cfb571
rmhde <rmhshopping@mac.com>
2021-12-02 13:22:00 +0100

    revising: app packaging using jpackage

--- Support/app-packaging/windows/prepare.cmd
+++ Support/app-packaging/windows/prepare.cmd
@@ -1,5 +1,5 @@
 set version=2.0.6-SNAPSHOT
 set licensedir=..\..\..\IDE\src\main\resources\Settings\LicenseIDE
-copy $licensedir\LICENSE + $licensedir\THIRD-PARTY.txt app-package\license.txt
+copy %licensedir%\LICENSE + %licensedir%\THIRD-PARTY.txt app-package\license.txt
 set jardir=..\..\..\IDE\target
-copy $ardir\*-win.jar /B app-package\jar\sikulixide-%version%.jar /B
+copy %jardir%\*-win.jar /B app-package\jar\sikulixide-%version%.jar /B
