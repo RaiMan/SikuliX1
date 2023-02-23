78aadd9e5724c296770ff4e9a9f07e305377bf7f
RaiMan <sikulix@outlook.com>
2023-02-23 14:26:19 +0100

    added: package org.sikuli.recorder: the place to implement a recording feature

---
+++ API/src/main/java/org/sikuli/recorder/RecorderWhatIsItFor.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2010-2022, sikuli.org, sikulix.com - MIT license
+ */
+
+package org.sikuli.recorder;
+
+/**
+ * This package is the place to implement a recording solution for user mouse and keyboard actions
+ * based on SikuliX's visual approach including playback and code generation for scripting
+ */
+public class RecorderWhatIsItFor {
+}
