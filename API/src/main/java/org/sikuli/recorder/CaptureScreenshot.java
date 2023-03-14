a3f1a078940647ed32874ab08e4272853c5fb75e
Joshua Spinak <jspinak@hotmail.com>
2023-03-14 15:13:01 +0100

    Change ScreenImage to Image

--- API/src/main/java/org/sikuli/recorder/CaptureScreenshot.java
+++ API/src/main/java/org/sikuli/recorder/CaptureScreenshot.java
@@ -1,5 +1,6 @@
 package org.sikuli.recorder;
 
+import org.sikuli.script.Image;
 import org.sikuli.script.Mouse;
 import org.sikuli.script.Screen;
 import org.sikuli.script.ScreenImage;
@@ -21,8 +22,8 @@ public class CaptureScreenshot {
 
     public void saveScreenshot(File directory, String baseFileName) {
         Screen activeScreen = Mouse.at().getMonitor();
-        ScreenImage screenshot = activeScreen.capture();
-        saveFile.saveWithDate(screenshot.getImage(), directory, baseFileName);
+        Image screenshot = activeScreen.getImage();
+        saveFile.saveWithDate(screenshot.get(), directory, baseFileName);
     }
 
 }
